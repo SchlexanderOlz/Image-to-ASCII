ea62767349545c27717399a8d4be0431a495c766
scholz200217 <scholz200217@sr.htlweiz.at>
2023-02-26 09:48:19 +0100

    Initial commit

---
+++ build/CMakeFiles/ASCII_Converter.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ASCII_Converter.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/startProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/startProject.dir/convert-to-ascii.cpp.obj"
+  "CMakeFiles/startProject.dir/convert-to-ascii.cpp.obj.d"
+  "libstartProject.dll.a"
+  "startProject.exe"
+  "startProject.exe.manifest"
+  "startProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/startProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
